f5c235c55aa13ea2470e5d868a61bfdb30a8cc2e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-19 14:45:15 +0600

    math function

--- StringMethod.cs
+++ StringMethod.cs
@@ -2,6 +2,10 @@ using System;
 namespace StringMethod
 {
     class Program{
-        
+        static void Main(string[] args){
+            string s = "hello worLd";
+            int x = s.Length;
+            Console.WriteLine("Length of String " + x);
+        }
     }
 }
