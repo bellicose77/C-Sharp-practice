257328304e2950971453c9481651384d315e6315
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-19 14:53:33 +0600

    string

--- StringMethod.cs
+++ StringMethod.cs
@@ -6,8 +6,10 @@ namespace StringMethod
             string s = "hello worLd";
             int x = s.Length;
             string upper = s.ToUpper();
+            string lower = s.ToLower();
             Console.WriteLine("Length of String " + x);
             Console.WriteLine("all upercase " + upper);
+            Console.WriteLine("all lower case " + lower);
         }
     }
 }
