4ca5447b52d50f203fef21bfe5de9e2996e60097
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-19 15:40:23 +0600

    loop

--- Loop.cs
+++ Loop.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace Loop{
     class Program{
-        
+        static void Main(string[] args){
+            int i =0;
+            while(i<5){
+                Console.WriteLine(i);
+                i++;
+            }
+        }
     }
 }
