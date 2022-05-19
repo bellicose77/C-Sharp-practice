45a91b2c18bb7a611c0096c9fdcdd96b58d7285b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-19 14:34:38 +0600

    math function

--- MathFunction.cs
+++ MathFunction.cs
@@ -6,7 +6,7 @@ namespace MathFunc
     {
         static void Main(string[] args){
             int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convertk.ToInt32(Console.ReadLine());
+            int y = UriTypeConverter.ToInt32(Console.ReadLine());
             if(x>y){
                 Console.WriteLine("X is greater than y");
             }
