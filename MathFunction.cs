3078a4351ae229a3bf1e251d340e6e126ac2c75b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-19 13:11:22 +0600

    math function

--- MathFunction.cs
+++ MathFunction.cs
@@ -2,5 +2,10 @@ using System;
 
 namespace MathFunc
 {
-    
+    class Program
+    {
+        static void Main(string[] args){
+            int x = Convert.ToInt32(Console.ReadLine());
+        }
+    }
 }
