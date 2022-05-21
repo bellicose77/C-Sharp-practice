31dea99e58065eb5f0fdc58bdcb6849a014be9a1
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-21 11:51:26 +0600

    overloading

--- MethodOverloading.cs
+++ MethodOverloading.cs
@@ -7,10 +7,16 @@ namespace Overloading
         static int myMethod(int x,int y){
             return x+y;
         }
+
+        static double myMethod(double x,double y){
+            return x+y;
+        }
         static void Main(string[] args)
         {
             int sum = myMethod(4,6);
+            double sum1 = myMethod(5.7,8.7);
             Console.WriteLine(sum);
+            Console.WriteLine(sum1);
 
         }
     }
