0db30813cc94fb75c34cacbd419ab93bb89dbadb
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-21 11:46:57 +0600

    overloading

---
+++ MethodOverloading.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Overloading
+{
+    class Program
+    {
+        static int myMethod(int x,int y){
+            return x+y;
+        }
+        static void Main(string[] args)
+        {
+            int sum = myMethod(4,6);
+        }
+    }
+}
