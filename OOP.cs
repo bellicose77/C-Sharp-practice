0508bb23e307d131439e2e155cd0925f4d03c211
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-22 15:08:45 +0600

    method create

--- OOP.cs
+++ OOP.cs
@@ -9,9 +9,16 @@ namespace OOP
         {
             Car myObj = new Car();
             Console.WriteLine(myObj.color);
+            myObj.full();
         }
     }
     class Car{
         public string color = "BLue";
+        int maxspeed = 20;
+
+        public void full()
+        {
+            Console.WriteLine("The car is going fast");
+        }
     }
 }
