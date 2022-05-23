649a926876540294adab0ebed447bf7e2f7ab928
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-23 13:58:57 +0600

    inheritance

--- Inheritance.cs
+++ Inheritance.cs
@@ -6,12 +6,23 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-
+            Car myObj = new Car();
+            myObj.Monk();
         }
     }
 
     class Vechle
     {
+        public string model = "ford";
+        public void Monk()
+        {
+            Console.WriteLine("Blba blba");
+        }
 
+    }
+
+    class Car : Vechle
+    {
+        public string model ="Mustag";
     }
 }
